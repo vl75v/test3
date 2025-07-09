77797ad69aa7546ac166bb13e73841fb2b691064
Vladimir Dudarev <vldudarev@yandex.ru>
2025-07-09 21:57:31 +0300

    добавлен ещё один блок

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const element = document.getElementById("root");
+element.innerHTML = `<div class='card'>Lorem ipsum,
+        dolor sit amet consectetur adipisicing elit.<br />
+        Nulla saepe ex, non cupiditate,<br />
+        hic ut earum in veritatis dolorum ad reprehenderit <br />
+        tempora quis qui pariatur voluptatem natus tenetur,<br />
+        veniam cumque.</div>`;
 const newElem = document.createElement("div");
 newElem.className = "card";
 newElem.textContent =
   "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla saepe ex, non cupiditate,hic ut earum in veritatis dolorum ad reprehenderit  tempora quis qui pariatur voluptatem natus tenetur, veniam cumque.";
-newElem.style.padding = "30px";
+newElem.style.padding = "20px";
 document.body.appendChild(newElem);
